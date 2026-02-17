7d29eb44e080054e887d1b1777a889145fa776be
Gourav <gouravbarwal001@gmail.com>
2026-02-17 23:57:31 +0530

    remove duplicacy

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -5,13 +5,13 @@ import toast from "react-hot-toast";
 // Login 
 export const loginUser = async (data) => {
     const response = await api.post("/api/auth/login", data);
-    toast.success("Login successful ✅");
+    // toast.success("Login successful ✅");
     return response.data;
 };
 
 // Register
 export const registerUser = async (data) => {
     const response = await api.post("/api/auth/register", data);
-    toast.success("Register successful ✅");
+    // toast.success("Register successful ✅");
     return response.data;
 };
